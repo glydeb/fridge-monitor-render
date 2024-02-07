a36b4b1e25038edb45c9c37ee34fe194f8a54ac8
Glydewell Burdick <glydewellb@gmail.com>
2024-02-07 12:31:07 -0600

    add error handling for Axios request

--- server/routes/api/alert.js
+++ server/routes/api/alert.js
@@ -16,10 +16,37 @@ router.post('/', async (req, res) => {
 
         console.log(response.data);
         res.status(200).json({ success: true });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: 'Failed to send Discord message' });
+    } catch (AxiosError) {
+        const { delay, status } = handleAxiosError(AxiosError);
+
+        console.log(`Retry in ${delay}s`);
+        const responseBody = {
+            success: false,
+            error: 'Failed to send Discord message',
+            retry: delay
+        };
+
+        res.status(status).json(responseBody);
     }
 });
+
+const handleAxiosError = (AxiosError) => {
+    let status = 500;
+    console.error(AxiosError.code);
+    if (AxiosError.response) {
+        console.error(AxiosError.response.data);
+        console.error(AxiosError.response.status);
+        console.error(AxiosError.response.headers);
+        status = AxiosError.response.status;
+    }
+
+    let delay = 6000;
+
+    if (AxiosError.response.status === 429) {
+        delay = AxiosError.response.headers['retry-after'];
+    };
+
+    return { delay, status };
+};
 
 module.exports = router;
